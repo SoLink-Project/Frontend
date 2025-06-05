ebaef677dc0adedecb9d81a1810b35f2884580ee
Sabolch <sabolch.dev@gmail.com>
2025-06-05 19:46:05 +0200

    Commit

--- src/banner.tsx
+++ src/banner.tsx
@@ -94,7 +94,7 @@ function Banner() {
             <div className="bg-white h-[800px]">
                 <div className="max-w-3xl flex flex-col mx-auto size-full">
                     <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
-                        <h1 className="block text-7xl font-bold text-gray-800 sm:text-9xl dark:text-white">Ads</h1>
+                        <h1 className="block text-5xl font-bold text-gray-800 sm:text-9x">Ads</h1>
                         <p className="mt-3 text-gray-600 dark:text-neutral-400">Here is an advertisement</p>
                         <p className="text-gray-600 dark:text-neutral-400">But there isn't at the moment</p>
                     </div>
